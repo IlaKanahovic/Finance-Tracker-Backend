29b67aa8f28617dfd7f5790f65323b7295cac78b
Ila_Kanahovic <computerilakanahovic@gmail.com>
2026-07-08 22:32:49 +0800

    feat: add PATCH /api/password for edit current password

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -41,29 +41,18 @@ export const changeEmail = async (req: Request, res: Response) => {
         const { email, password } = req.body
         const userId = (req as any).userId
 
-        if (!email) {
-            return res.status(400).json({ message: 'Email обязателен' })
-        }
-        if (!password) {
-            return res.status(400).json({ message: 'Пароль обязателен' })
-        }
+        if (!email) return res.status(400).json({ message: 'Email обязателен' })
+        if (!password) return res.status(400).json({ message: 'Пароль обязателен' })
 
         const user = await prisma.user.findUnique({
             where: { id: userId }
         })
 
-        if (!user) {
-            return res.status(404).json({ message: 'Пользователь не найден' })
-        }
-
-        if (user.email === email) {
-            return res.status(400).json({ message: 'Новый email совпадает с текущим' })
-        }
+        if (!user) return res.status(404).json({ message: 'Пользователь не найден' })
+        if (user.email === email) return res.status(400).json({ message: 'Новый email совпадает с текущим' })
 
         const isPasswordValid = await bcrypt.compare(password, user.password)
-        if (!isPasswordValid) {
-            return res.status(401).json({ message: 'Неверный пароль' })
-        }
+        if (!isPasswordValid) return res.status(401).json({ message: 'Неверный пароль' })
 
         const checkValidEmail = await prisma.user.findFirst({
             where: {
@@ -71,9 +60,7 @@ export const changeEmail = async (req: Request, res: Response) => {
                 NOT: { id: userId }
             }
         })
-        if (checkValidEmail) {
-            return res.status(409).json({ message: 'Пользователь с таким email уже существует' })
-        }
+        if (checkValidEmail) return res.status(409).json({ message: 'Пользователь с таким email уже существует' })
 
         const emailUpdate = await prisma.user.update({
             where: { id: userId },
@@ -82,6 +69,40 @@ export const changeEmail = async (req: Request, res: Response) => {
 
         res.json({ email: email })
 
+    } catch (error) {
+        console.error('Error change email', error)
+        res.status(500).json({ message: 'Ошибка сервера' })
+    }
+}
+
+export const changePassword = async (req: Request, res: Response) => {
+    try {
+        const { currentPassword, newPassword } = req.body
+        const userId = (req as any).userId
+
+        if (!currentPassword) return res.status(400).json({ message: 'Текущий пароль обязателен' })
+        if (!newPassword) return res.status(400).json({ message: 'Новый пароль обязателен' })
+
+        const user = await prisma.user.findUnique({
+            where: { id: userId }
+        })
+
+        if (!user) return res.status(404).json({ message: 'Пользователь нот фаунд' })
+
+        const isSamePassword = await bcrypt.compare(newPassword, user.password)
+        if (isSamePassword) return res.status(400).json({ message: 'Новый пароль совпадает с текущим' })
+
+        const validPassword = await bcrypt.compare(currentPassword, user.password)
+        if (!validPassword) return res.status(401).json({ message: 'Неверный пароль :(' })
+
+        const hashedPassword = await bcrypt.hash(newPassword, 10)
+
+        const passwordUpdate = await prisma.user.update({
+            where: { id: userId },
+            data: { password: hashedPassword }
+        })
+
+        res.json({ newPassword: newPassword })
     } catch (error) {
         console.error('Error change email', error)
         res.status(500).json({ message: 'Ошибка сервера' })
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { register, login } from '../controllers/authController'
-import { changeEmail, changeName } from '../controllers/userController'
+import { changeEmail, changeName, changePassword } from '../controllers/userController'
 import { verifyToken } from '../middleware/authMiddleware'
 
 const router = Router()
@@ -9,5 +9,6 @@ router.post('/register', register)
 router.post('/login', login)
 router.patch('/user', verifyToken, changeName)
 router.patch('/email', verifyToken, changeEmail)
+router.patch('/password', verifyToken, changePassword)
 
 export default router
