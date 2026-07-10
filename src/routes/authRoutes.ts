febd2faa9bab07372ecf9a7086356e11c1e9a065
Ila_Kanahovic <computerilakanahovic@gmail.com>
2026-07-11 01:23:01 +0800

    feat: add /api/delete for delete all transactions v2

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import { register, login } from '../controllers/authController'
 import { changeEmail, changeName, changePassword } from '../controllers/userController'
 import { verifyToken } from '../middleware/authMiddleware'
+import { deleteAllTransactions } from '../controllers/transactionsController'
 
 const router = Router()
 
@@ -10,5 +11,6 @@ router.post('/login', login)
 router.patch('/user', verifyToken, changeName)
 router.patch('/email', verifyToken, changeEmail)
 router.patch('/password', verifyToken, changePassword)
+router.delete('/delete', verifyToken, deleteAllTransactions)
 
 export default router
